ede735ed5464e29490cf6c1fe5913e5aafc5461c
jaspershen <shenxt1990@outlook.com>
2024-01-06 20:52:26 -0800

    update

--- 1-code/1-data_preparation/1-survey_data_preparation_6_points.py
+++ 1-code/1-data_preparation/1-survey_data_preparation_6_points.py
@@ -13,3 +13,32 @@ data = pd.read_csv(file_path)
 
 # Print the data
 print(data)
+
+data
+
+###how to get the column names
+data.columns
+
+###how to get the row names
+data.index
+
+###how to get the data types
+data.dtypes
+
+##how to get the shape of the data
+data.shape
+
+##how to get the summary statistics
+data.describe()
+
+###how to get the first 5 rows
+data.head()
+
+###how to get the first 10 rows
+data.head(10)
+
+###how to get the last 5 rows
+data.tail()
+
+###how to get the first row
+data.iloc[0]
